d0716c3b9214167e63ee6a39568e1ac558eaba11
Cyril CAPBERN <ccapbern@asulia.fr>
2016-04-25 16:58:25 +0200

    ADD: AbstractJpaRepository and IoEntity

---
+++ src/main/java/fr/iocean/application/persistence/IoEntity.java
@@ -0,0 +1,10 @@
+package fr.iocean.application.persistence;
+
+import java.io.Serializable;
+
+public interface IoEntity extends Serializable {
+
+	Long getId();
+
+	void setId(Long id);
+}
---
+++ src/main/java/fr/iocean/application/repository/AbstractJpaRepository.java
@@ -0,0 +1,69 @@
+package fr.iocean.application.repository;
+
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import org.hibernate.Session;
+
+import fr.iocean.application.persistence.IoEntity;
+
+public abstract class AbstractJpaRepository<T extends IoEntity> {
+
+    protected Class<T> entityClass;
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @PostConstruct
+    public void init() {
+        entityClass = getEntityClass();
+    }
+
+    protected abstract Class<T> getEntityClass();
+
+    protected Session getSession() {
+        return em.unwrap(Session.class);
+    }
+
+    @Transactional
+    public T save(T entity) {
+        if (isNew(entity)) {
+            em.persist(entity);
+            return entity;
+        } else if (!em.contains(entity)) {
+            return em.merge(entity);
+        }
+
+        return entity;
+    }
+
+    @Transactional
+    public T findOne(Long id) {
+        return em.find(entityClass, id);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Transactional
+    public List<T> findAll() {
+        return getSession().createCriteria(entityClass).list();
+    }
+
+    @Transactional
+    public void delete(T entity) {
+        if (!getSession().contains(entity)) {
+            em.remove(getSession().merge(entity));
+        } else {
+            em.remove(entity);
+        }
+
+    }
+
+    private boolean isNew(T entity) {
+        return entity.getId() == null;
+    }
+
+}
